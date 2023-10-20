b9859b37e0740f9237ebb9c91ec50ba890da9412
rafaelflk <146246231+rafaelflk@users.noreply.github.com>
2023-10-20 18:56:45 -0300

    Add files via upload

---
+++ Prova01.py
@@ -0,0 +1,13 @@
+# * Crie um exemplo de cada uma das principais tipagens de variáveis.
+# * Crie uma solicitação de dados ao usuário.
+# * Imprima a solicitação anterior na tela com uma mensagem personalizada.
+
+nome = input('Digite seu nome completo: ')
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura em metros: '))
+
+print('Seu nome é: ', nome)
+print('Sua idade é: ', idade,' anos.')
+print('Sua altura é: ', altura,' metros.')
+               
+
